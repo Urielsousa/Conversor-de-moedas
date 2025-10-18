d4d8677356913fcad09c16e56f23b50c7c3eb3c2
Urielsousa <urielzinho@hotmail.com>
2025-10-18 16:52:37 -0300

    final project

--- scripts.js
+++ scripts.js
@@ -32,15 +32,18 @@ function convertValue() {
 }
 function changeCurrency() {
   const currencyName = document.getElementById("currency-name");
-  const currencyImage = document.querySelector(".currency-img")
+  const currencyImage = document.querySelector(".currency-img");
   if (currencySelect.value == "dolar") {
     currencyName.innerHTML = "Dólar americano";
-    currencyImage.src = "./assets/d"
+    currencyImage.src = "./assets/dolar.png";
   }
 
   if (currencySelect.value == "euro") {
     currencyName.innerHTML = "Euro";
+    currencyImage.src = "./assets/euro.png";
   }
+
+  convertValue();
 }
 
 currencySelect.addEventListener("change", changeCurrency);
